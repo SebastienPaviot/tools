f767bdbc055093eb04969597f28d9748e584e5fa
SebastienPaviot <43282316+SebastienPaviot@users.noreply.github.com>
2024-10-26 08:37:46 +0200

    v

--- tools/js/ai/webllm/hellochat/hellochat.js
+++ tools/js/ai/webllm/hellochat/hellochat.js
@@ -1,6 +1,6 @@
 import * as webllm from "https://esm.run/@mlc-ai/web-llm";
 
-console.log("hellochat v0.1");
+console.log("hellochat v0.8");
 const availableModels = webllm.prebuiltAppConfig.model_list.map(
   (m) => m.model_id,
 );
