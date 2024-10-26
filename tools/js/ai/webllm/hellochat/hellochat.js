9dd192d41e2969e774b242306d622462c5efe094
SebastienPaviot <43282316+SebastienPaviot@users.noreply.github.com>
2024-10-26 23:05:28 +0200

    k

--- tools/js/ai/webllm/hellochat/hellochat.js
+++ tools/js/ai/webllm/hellochat/hellochat.js
@@ -62,8 +62,10 @@ async function initializeWebLLMEngine() {
   console.log(selectedModel);
   const config = {
     temperature: 0.6,
-    top_p: 0.9,
-    max_tokens: 75, // Limite de sortie correspondant à environ 50 mots
+      top_p: 0.9,
+      max_tokens: 2000,
+      presence_penalty: 0.0, 
+      frequency_penalty: 0.0, // Limite de sortie correspondant à environ 50 mots
   };
   console.log(config);
   await engine.reload(selectedModel, config);
@@ -82,9 +84,9 @@ async function streamingGenerating(messages, onUpdate, onFinish, onError) {
       stream: true,
       temperature: 0.6,
       top_p: 0.9,
-     // max_tokens: 2000,
-     // presence_penalty: 0.0, 
-     // frequency_penalty: 0.0,
+      max_tokens: 2000,
+      presence_penalty: 0.0, 
+      frequency_penalty: 0.0,
       messages,
       stream_options: { include_usage: true },
     });
