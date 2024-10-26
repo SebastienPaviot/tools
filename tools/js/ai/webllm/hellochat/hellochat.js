ce088a443557217b50a759e5e33b688cccecbc4b
SebastienPaviot <43282316+SebastienPaviot@users.noreply.github.com>
2024-10-26 10:58:40 +0200

    Update hellochat.js

--- tools/js/ai/webllm/hellochat/hellochat.js
+++ tools/js/ai/webllm/hellochat/hellochat.js
@@ -45,6 +45,7 @@ engine.setInitProgressCallback(updateEngineInitProgressCallback);
 async function initializeWebLLMEngine() {
   document.getElementById("download-status").classList.remove("hidden");
   //selectedModel = document.getElementById("model-selection").value;
+  console.log(selectedmodel);
   const config = {
     temperature: 1.0,
     top_p: 1,
