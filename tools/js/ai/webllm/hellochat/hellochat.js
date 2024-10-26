ca28cb8c8a333a520b93d0a1e12a1e5cc7ac833d
SebastienPaviot <43282316+SebastienPaviot@users.noreply.github.com>
2024-10-26 08:02:33 +0200

    ui

--- tools/js/ai/webllm/hellochat/hellochat.js
+++ tools/js/ai/webllm/hellochat/hellochat.js
@@ -7,3 +7,21 @@ const availableModels = webllm.prebuiltAppConfig.model_list.map(
 console.log(availableModels);
 
 let selectedModel = "Llama-3.1-8B-Instruct-q4f32_1-1k";
+
+
+/*************** UI binding ***************/
+availableModels.forEach((modelId) => {
+  const option = document.createElement("option");
+  option.value = modelId;
+  option.textContent = modelId;
+  document.getElementById("model-selection").appendChild(option);
+});
+document.getElementById("model-selection").value = selectedModel;
+document.getElementById("download").addEventListener("click", function () {
+  initializeWebLLMEngine().then(() => {
+    document.getElementById("send").disabled = false;
+  });
+});
+document.getElementById("send").addEventListener("click", function () {
+  onMessageSend();
+});
