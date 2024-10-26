a347a6e4758c94ef3889439508d090c5d2cc9977
SebastienPaviot <43282316+SebastienPaviot@users.noreply.github.com>
2024-10-26 07:55:47 +0200

    Update hellochat.js

--- tools/js/ai/webllm/hellochat/hellochat.js
+++ tools/js/ai/webllm/hellochat/hellochat.js
@@ -1,1 +1,6 @@
 import * as webllm from "https://esm.run/@mlc-ai/web-llm";
+
+const availableModels = webllm.prebuiltAppConfig.model_list.map(
+  (m) => m.model_id,
+);
+let selectedModel = "Llama-3.1-8B-Instruct-q4f32_1-1k";
