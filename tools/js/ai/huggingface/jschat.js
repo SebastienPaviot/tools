2f0ede2a6672f2df36788754a62d01c9e3a7ece6
SebastienPaviot <43282316+SebastienPaviot@users.noreply.github.com>
2024-10-27 07:39:36 +0100

    8

--- tools/js/ai/huggingface/jschat.js
+++ tools/js/ai/huggingface/jschat.js
@@ -55,7 +55,7 @@ divlog(out);
 // using pipeline function
 pipe = await pipeline('text-generation', 'Xenova/Qwen1.5-0.5B-Chat', {model_file_name: 'decoder_model_merged'})
 // using AutoModel class
-let model = await AutoModel.from_pretrained('Xenova/Qwen1.5-0.5B-Chat', {model_file_name:'decoder_model_merged'})
+//let model = await AutoModel.from_pretrained('Xenova/Qwen1.5-0.5B-Chat', {model_file_name:'decoder_model_merged'})
 // will fetch decoder_model_merged_quantized.onnx
 
 
